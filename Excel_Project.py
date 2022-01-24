58ed71e9c0221bd09a0c7e4c8af61d6d728f3ffb
Jay <jay.sanjay.karia@gmail.com>
2022-01-24 20:31:53 +0530

    task 4 done
    last 4 digits of bank account done

--- Excel_Project.py
+++ Excel_Project.py
@@ -5,20 +5,39 @@ import json
 input_file_name = "Template.xlsx"
 output_file_name = "Final.xlsx"
 
+account_numbers_list = []
+last_4_digits = []
+
+workbook = load_workbook(input_file_name)
+worksheet = workbook['Deposits']
+
 def ReadJSONData():
+    global account_numbers_list
+    global last_4_digits
+
     with open("CE_Analytics.json") as json_file:
         data = json.load(json_file)
         # Getting the total account numbers
         bank_accounts = data['response']['bank_accounts']
+        # Update the excel file with json bank_accounts number and replacing with F2 block in excel
         total_bank_accounts = len(bank_accounts)
-        # Update the excel file with json bank_accounts number
-        workbook = load_workbook(input_file_name)
-        worksheet = workbook['Deposits']
         worksheet['F2'] = total_bank_accounts
-        workbook.save(output_file_name)
         # Getting the last 4 digits of account number
         for i in range(0, total_bank_accounts):
             account_number = data['response']['bank_accounts'][i]['account_number']
-            last_4 = account_number[len(account_number)-1]+account_number[len(account_number)-2]+account_number[len(account_number)-3]+account_number[len(account_number)-4]
+            last_4_digits.append(account_number[len(account_number)-4]+account_number[len(account_number)-3]+account_number[len(account_number)-2]+account_number[len(account_number)-1])
+            account_numbers_list.append(last_4_digits[i])
+            WriteAccountNo(account_numbers_list)
+            workbook.save(output_file_name)
+
+def WriteAccountNo(account_numbers):
+    if len(account_numbers) == 1:
+        worksheet['G5'] = account_numbers[0]
+    elif len(account_numbers) == 2:
+        worksheet['G28'] = account_numbers[1]
+    elif len(account_numbers) == 3:
+        worksheet['G51'] = account_numbers[2]
+    elif len(account_numbers) == 4:
+        worksheet['G74'] = account_numbers[3]
 
 ReadJSONData()
